1abf2e4a59c6f2027c994104e12fe0b9377807c1
wowankz <wowikwow@mail.ru>
2021-06-26 22:42:29 +0600

    add avatar

--- src/panels/catalog/Catalog.js
+++ src/panels/catalog/Catalog.js
@@ -1,24 +1,33 @@
 import React from 'react';
 
 
-import { Panel, PanelHeader, PanelHeaderBack, Avatar, Div } from '@vkontakte/vkui';
+import { Panel, PanelHeader, PanelHeaderBack, Avatar, Div, ContentCard, CardGrid } from '@vkontakte/vkui';
 
 
 import './catalog.css';
-
+import mainNewsImg from '../../img/news.png'
 const Catalog = props => (
-	<Panel id={props.id}>
+	<Panel id={props.id} className="catalog">
 		<PanelHeader
+			className="header"
 			separator={false}
 			left={<PanelHeaderBack className="back" onClick={props.goBack} data-to="home" />}
 		>
-			<Div className="header">
-				<Avatar src={props.fetchedUser.photo_200} />
-			</Div>
+
+			<Avatar width="48" className="header-avatar" src={props.fetchedUser.photo_200} />
 
 		</PanelHeader>
-
-	</Panel>
+		<CardGrid size="l">
+			<ContentCard
+				image={mainNewsImg}
+				subtitle="unsplash"
+				header="persons left hand with pink paint"
+				text="Five hours of makeup and paint to achieve the human anatomy photoshoot. Thank you Steph and Shay. See more and official credit on @jawfox.photography."
+				caption="Photo by Alexander Jawfox on Unsplash"
+				maxHeight={423}
+			/>
+		</CardGrid>
+	</Panel >
 );
 
 export default Catalog;
