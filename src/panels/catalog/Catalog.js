865862c19804c3a2b7a52577da358d89a3e4ae4e
wowankz <wowikwow@mail.ru>
2021-06-27 02:16:39 +0600

    ..............

--- src/panels/catalog/Catalog.js
+++ src/panels/catalog/Catalog.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-import { Panel, PanelHeader, PanelHeaderBack, Avatar, Div, Card, CardGrid, Title, Text, Headline, Group, CellButton } from '@vkontakte/vkui';
+import { Panel, PanelHeader, PanelHeaderBack, Avatar, Div, Card, CardGrid, Title, Text, Headline, Group, Button } from '@vkontakte/vkui';
 import { Icon12Chevron } from '@vkontakte/icons';
 
 import './catalog.css';
@@ -26,15 +26,15 @@ const Catalog = props => (
 		</Group>
 
 		<CardGrid size="l" >
-			<Card className="lottery" size="l" mode="shadow" style={{ paddingLeft: 25, paddingRight: 10, paddingTop: 25, paddingBottom: 16, marginTop: 56 }}>
+			<Card className="lottery" size="l" mode="shadow" >
 				<Headline weight="semibold" style={{ textAlign: 'center', marginBottom: 30 }} >Розыгрышь дня!</Headline>
-				<Group onClick={() => { console.log('Open News'); }} role="link">
+				<Div className="lottery-items">
 					<Div className="lottery-item">
-						<img width="150" height="69" style={{ borderRadius: 3 }} src={mainNewsImg} alt="lottery-images" />
-						<Div style={{ paddingRight: 0, paddingTop: 0 }}>
-							<Headline weight="semibold" style={{ fontSize: 14 }}>Пройди тест и получи</Headline>
+						<img width="45%" height="20%" style={{ borderRadius: 3 }} src={mainNewsImg} alt="lottery-images" />
+						<Div style={{ paddingRight: 0, paddingTop: 0, paddingBottom: 0, overflow: 'hidden' }}>
+							<Headline weight="semibold" style={{ fontSize: 14, textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden' }}>Пройди тест и получи</Headline>
 							<Text style={{ fontSize: 14 }} weight="regular">Ак 47 Вулкан</Text>
-							<Div style={{ fontSize: 14, display: 'flex', alignItems: 'center', padding: 0, marginTop: 10 }}>
+							<Div style={{ fontSize: 14, display: 'flex', alignItems: 'center', padding: 0, marginTop: 10, color: '#2B282D' }} onClick={() => { console.log('Open lottery'); }} role="link">
 								Подробнее
 								<Icon12Chevron />
 							</Div>
@@ -42,17 +42,23 @@ const Catalog = props => (
 					</Div>
 
 					<Div className="lottery-item">
-						<img width="150" height="69" style={{ borderRadius: 3 }} src={mainNewsImg} alt="lottery-images" />
-						<Div style={{ paddingRight: 0, paddingTop: 0 }}>
-							<Headline weight="semibold" style={{ fontSize: 14 }}>Пройди тест и получи</Headline>
+						<img width="45%" height="20%" style={{ borderRadius: 3 }} src={mainNewsImg} alt="lottery-images" />
+						<Div style={{ paddingRight: 0, paddingTop: 0, paddingBottom: 0, overflow: 'hidden' }}>
+							<Headline weight="semibold" style={{ fontSize: 14, textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden' }}>Пройди тест и получи</Headline>
 							<Text style={{ fontSize: 14 }} weight="regular">Ак 47 Вулкан</Text>
-							<Div style={{ fontSize: 14, display: 'flex', alignItems: 'center', padding: 0, marginTop: 10 }}>
+							<Div style={{ fontSize: 14, display: 'flex', alignItems: 'center', padding: 0, marginTop: 10, color: '#2B282D' }} onClick={() => { console.log('Open lottery'); }} role="link">
 								Подробнее
 								<Icon12Chevron />
 							</Div>
 						</Div>
 					</Div>
-				</Group>
+
+					<Button size="m" onClick={props.go} data-to="catalog" style={
+						{
+							fontSize: 12, lineHeight: 20, backgroundColor: ' #BD00FF', borderRadius: 50,
+							marginTop: 40, paddingBottom: 5, paddingTop: 5, paddingRight: 17, paddingLeft: 17
+						}}>Больше розыгрышей</Button>
+				</Div>
 			</Card>
 		</CardGrid>
 	</Panel >
